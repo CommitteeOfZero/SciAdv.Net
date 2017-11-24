da634975780706e4e9c2cce8fd251846a084a084
Niklas K <drdaxxy@googlemail.com>
2017-11-24 20:06:26 +0000

    Remove pause in build.cmd for automation
    Signed-off-by: Evgeny Ivchenko <goldenjoe@outlook.com>

--- build.cmd
+++ build.cmd
@@ -6,4 +6,3 @@ set msbuild=%VSPATH%\MSBuild\15.0\Bin\MSBuild.exe
 
 "%msbuild%" SciAdvNet.sln /t:restore /p:Configuration=Release
 "%msbuild%" SciAdvNet.sln /p:Configuration=Release /nologo /maxcpucount /m /v:m
-pause
